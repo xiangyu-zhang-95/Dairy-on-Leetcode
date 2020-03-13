d7735bbf9f68f0510672e197df687e6239b725b1
Xiangyu Zhang <35207232+xiangyu-zhang-95@users.noreply.github.com>
2020-03-13 11:31:18 -0400

    debug heap implementation using Leetcode 23

--- heap.py
+++ heap.py
@@ -3,22 +3,22 @@ class heap:
 		self.is_min = True if s == "min" else False
 		self.a = []
 	
-	def _sift_up(n):
-		while n >= 0:
+	def _sift_up(self,n):
+		while n > 0:
 			m = (n - 1) // 2
-			if (a[n] < a[m] and self.is_min) or (a[n] >= a[m] and not self.is_min):
-				a[n], a[m] = a[m], a[n]
+			if (self.a[n] < self.a[m] and self.is_min) or (self.a[n] >= self.a[m] and not self.is_min):
+				self.a[n], self.a[m] = self.a[m], self.a[n]
 				n = m
 				continue
 			else:
 				break
 	
-	def _sift_down(n):
+	def _sift_down(self,n):
 		left, right = 2 * n + 1, 2 * n + 2
 		while left <= len(self.a) - 1:
 			l_val = self.a[left]
 			if right == len(self.a):
-				if (self.min and self.a[n] <= l_val) or (self.max and self.a[n] > l_val):
+				if (self.is_min and self.a[n] <= l_val) or ((not self.is_min) and self.a[n] > l_val):
 					break
 				self.a[n], self.a[left] = self.a[left], self.a[n]
 				break
@@ -26,45 +26,33 @@ class heap:
 			min_child = left if self.a[left] <= self.a[right] else right
 			max_child = right if self.a[left] <= self.a[right] else left
 			if self.is_min:
-				if self.a[n] <= min_child:
+				if self.a[n] <= self.a[min_child]:
 					break
-				self.a[n], self.a[min] = self.a[min], self.a[n]
-				n, left, right = min, 2 * min + 1, 2 * min + 2
+				self.a[n], self.a[min_child] = self.a[min_child], self.a[n]
+				n, left, right = min_child, 2 * min_child + 1, 2 * min_child + 2
 				continue
 
 			if self.a[max_child] <= self.a[n]:
 				break
-			self.a[n], self.a[max] = self.a[max], self.a[n]
-			n, left, right = max, 2 * max + 1, 2 * max + 2
+			self.a[n], self.a[max_child] = self.a[max_child], self.a[n]
+			n, left, right = max_child, 2 * max_child + 1, 2 * max_child + 2
 	
-	def push(v):
+	def push(self,v):
 		self.a.append(v)
 		self._sift_up(len(self.a) - 1)
 	
-	def pop():
+	def pop(self):
 		if not self.a:
 			raise Exception("pop empty heap")
 		if len(self.a) == 1:
-			a.pop()
-			return
+			return self.a.pop()
 
-		v = self.a.pop()
+		v, head = self.a.pop(), self.a[0]
 		self.a[0] = v
 		self._sift_down(0)
+		return head
 	
-	def peek():
+	def peek(self):
 		if not self.a:
 			raise Exception("peek empty heap")
 		return self.a[0]
-
-	
-
-
-
-
-				
-				
-		
-		
-	
-	
