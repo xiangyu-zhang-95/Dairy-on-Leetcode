0d798f21b45f8300ace361e6ec168fe7d783dc61
Xiangyu Zhang <35207232+xiangyu-zhang-95@users.noreply.github.com>
2020-04-04 11:00:21 -0400

    Update hashheap.py

--- Python/hashheap.py
+++ Python/hashheap.py
@@ -4,17 +4,17 @@ class hashheap:
 		self.a = []
 		self.map = {}
 	
-	def peek():
-		retuen self.a[0]
+	def peek(self):
+		return self.a[0]
 	
-	def _sift_up(i):
+	def _sift_up(self, i):
 		while i > 0 and (self.a[(i - 1) // 2] < self.a[i] if self.is_min else self.a[(i - 1) // 2] > self.a[i]):
 			self.a[i], self.a[(i - 1) // 2] = self.a[(i - 1) // 2], self.a[i]
 			self.map[self.a[i]], self.map[self.a[(i - 1)//2]] = i, (i - 1)//2
 			i = (i - 1) // 2
 		return i
 	
-	def _sift_down(i):
+	def _sift_down(self, i):
 		while 2 * i + 2 <= len(self.a) - 1 and self.a[i] > min(self.a[2 * i + 1], self.a[2 * i + 2]) if self.is_min else self.a[i] < max(self.a[2 * i + 1], self.a[2 * i + 2]):
 			if self.is_min:
 				p = 2 * i + 1 if self.a[2 * i + 1] < self.a[2 * i + 2] else 2 * i + 2
@@ -31,12 +31,12 @@ class hashheap:
 			i = 2 * i + 1
 		return i
 	
-	def push(v):
+	def push(self, v):
 		self.a.append(v)
 		self.map[v] = len(self.a) - 1
 		self._sift_up(len(self.a) - 1)
 	
-	def pop():
+	def pop(self):
 		returnitem = self.a[0]
 		if len(self.a) == 1:
 			self.a = []
@@ -51,7 +51,7 @@ class hashheap:
 		self._sift_down(0)
 		return returnitem
 	
-	def delete(v):
+	def delete(self, v):
 		i = self.map[v]
 		if i == len(self.a) - 1:
 			self.a.pop()
